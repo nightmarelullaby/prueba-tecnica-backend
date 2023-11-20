97c88cbfadb79ab90d71e61dd42f9b9a330b4a52
nightmarelullaby <videotuto32@gmail.com>
2023-11-20 10:31:33 -0400

    iddto added

--- src/domain/dtos/id.dto.ts
+++ src/domain/dtos/id.dto.ts
@@ -5,12 +5,12 @@ export class idDTO{
         public id:string,
     ){}
 
-    static create(required:boolean,id?:any|unknown):[string?,string?]{
-        if(!id && required) return ["Id is missing",undefined]
-        if(!id && !required) return [undefined,id]
-        if(typeof id !== "string") return ["Id is not a string",undefined]
-        if(!validate(id)) return ["Id is invalid",undefined];
-        return [undefined,String(id)]
+    static create(required:boolean,id?:any|unknown):[string,string?]{
+        if(!id && required) return [id,"Id is missing"]
+        if(!id && !required) return [id,undefined,]
+        if(typeof id !== "string") return [id,"Id is not a string",]
+        if(!validate(id)) return [id,"Id is invalid"];
+        return [String(id),undefined]
         
     }
 }
--- src/presentation/schedule/controller.ts
+++ src/presentation/schedule/controller.ts
@@ -9,7 +9,7 @@ export class ScheduleController{
         private readonly scheduleRepository:ScheduleRepository
     ){}
     getSchedule = (req:Request,res:Response) => {
-        const [error,id] = idDTO.create(false,req.query.q,)
+        const [id,error] = idDTO.create(false,req.query.q,)
         if(error) return res.status(400).json("Bad request")
         return this.scheduleRepository.get(id)
             .then(response => {
@@ -37,9 +37,9 @@ export class ScheduleController{
         })
     }
     deleteSchedule = (req:Request,res:Response) =>{
-        const [error,id] = idDTO.create(true,req.params.id)
+        const [id,error] = idDTO.create(true,req.params.id)
         console.log(error)
-        if(!id) return res.status(400).json("Bad request")
+        if(error) return res.status(400).json("Bad request")
         return this.scheduleRepository.delete(id)
             .then(response => {
             res.json(response)
